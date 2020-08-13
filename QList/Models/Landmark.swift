c23549fe4a7ff78f48b4c773884187c038f39a5f
durban.zhang <durban.zhang@gmail.com>
2020-08-13 23:05:51 +0800

    how to add list row

--- QList/Models/Landmark.swift
+++ QList/Models/Landmark.swift
@@ -10,7 +10,7 @@ import Foundation
 import CoreLocation
 import SwiftUI
 
-struct Landmark: Hashable, Codable {
+struct Landmark: Hashable, Codable, Identifiable {
     var id: Int
     var name: String
     fileprivate var imageName: String
