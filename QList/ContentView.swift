a8337cf28f6b348e10f154446346a2e6ba2be07b
durban.zhang <durban.zhang@gmail.com>
2020-08-14 23:09:28 +0800

    update ui - update content view

--- QList/ContentView.swift
+++ QList/ContentView.swift
@@ -9,42 +9,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var name: String = ""
-    @State var keywords: String = ""
-    @State var name1: String = ""
-    @State var name2: String = ""
-    
     var body: some View {
-        HStack{
-            VStack {
-                TextField("Input Your Name", text: $name)
-                List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
-                    Text("输入框位置")
-                        .edgesIgnoringSafeArea(.top)
-                        .frame(height: 20.0)
+        NavigationView{
+            List(landmarkData){ landmark in
+                NavigationLink(destination: LandmarkDetail()){
+                    LandmarkRow(landmark: landmark)
                 }
                 
-                Spacer()
             }
-            .frame(width: 200.0)
-            
-            Divider()
-            
-            VStack {
-                TextField("Input Your Keywords", text: $keywords)
-                
-                List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
-                    Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
-                        .edgesIgnoringSafeArea(.top)
-                }
-                
-                Spacer()
-            }
-            .frame(width: 600.0)
-
         }
+        .navigationBarTitle("Landmarks")
     }
 }
+
+//struct ContentView: View {
+//    @State var name: String = ""
+//    @State var keywords: String = ""
+//    @State var name1: String = ""
+//    @State var name2: String = ""
+//
+//    var body: some View {
+//        HStack{
+//            VStack {
+//                TextField("Input Your Name", text: $name)
+//                List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+//                    Text("输入框位置")
+//                        .edgesIgnoringSafeArea(.top)
+//                        .frame(height: 20.0)
+//                }
+//
+//                Spacer()
+//            }
+//            .frame(width: 200.0)
+//
+//            Divider()
+//
+//            VStack {
+//                TextField("Input Your Keywords", text: $keywords)
+//
+//                List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+//                    Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+//                        .edgesIgnoringSafeArea(.top)
+//                }
+//
+//                Spacer()
+//            }
+//            .frame(width: 600.0)
+//
+//        }
+//    }
+//}
 
 
 struct ContentView_Previews: PreviewProvider {
