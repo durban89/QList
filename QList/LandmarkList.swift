647dea1b95a38f56cc03e1345cfe8514c48021d8
durban.zhang <durban.zhang@gmail.com>
2020-08-17 22:40:04 +0800

    add favorite with if condition

--- QList/LandmarkList.swift
+++ QList/LandmarkList.swift
@@ -32,6 +32,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone SE", "iPhone XS MX"], id: \.self) { deviceName in
+            LandmarkList()
+            .previewDevice(PreviewDevice(rawValue: deviceName))
+            .previewDisplayName(deviceName)
+        }
     }
 }
