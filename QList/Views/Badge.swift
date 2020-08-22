4575332b832a9bbc4284ef4aa564c7dd9f4b0051
durban.zhang <durban.zhang@gmail.com>
2020-08-22 23:45:32 +0800

    add QuadCurve

--- QList/Views/Badge.swift
+++ QList/Views/Badge.swift
@@ -13,7 +13,12 @@ struct Badge: View {
         Path { path in
             let width: CGFloat = 100.0
             let height = width
-            path.move(to: CGPoint(x: width * 0.95, y: height * 0.20))
+            path.move(
+                to: CGPoint(
+                    x: width * 0.95,
+                    y: height * (0.20 + HexagonParameters.adjustment)
+                )
+            )
             
             HexagonParameters.points.forEach {
                 path.addLine(
@@ -22,6 +27,16 @@ struct Badge: View {
                         y: height * $0.useHeight.0 * $0.yFactors.0
                     )
                 )
+                
+                path.addQuadCurve(
+                    to: .init(
+                        x: width * $0.useWidth.1 * $0.xFactors.1,
+                        y: height * $0.useHeight.1 * $0.yFactors.1
+                    ),
+                    control: .init(
+                        x: width * $0.useWidth.2 * $0.xFactors.2,
+                        y: height * $0.useHeight.2 * $0.yFactors.2
+                    ))
             }
         }
         .fill(Color.black)
